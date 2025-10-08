acc86880d4f4b555540027cbde87f44d488afc99
Jonathan St-Michel <j.stmichel@icloud.com>
2025-10-08 10:57:49 -0400

    feat(ui): add navigation and layout enhancements
    * implement main content area with navigation links
    * add footer to layout
    * create layout overrides for better styling

--- src/Beagl.WebApp/Constants/LocalRedirection.cs
+++ src/Beagl.WebApp/Constants/LocalRedirection.cs
@@ -7,4 +7,7 @@ internal sealed class LocalRedirection
     public const string Login = "/Login";
     public const string Register = "/Register";
     public const string Index = "/Index";
+    public const string Users = "/Users/Index";
+    public const string UserCreation = "/Users/Create";
+    public const string UserEdition = "/Users/Edit";
 }
